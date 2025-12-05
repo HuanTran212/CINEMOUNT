797203fcc9f6caa20512b18b53f0231a9d850175
m-hung <minhhungthai7@gmail.com>
2025-12-05 14:39:25 +0700

    upChonPhim

--- BTL_TKWEB/js/main.js
+++ BTL_TKWEB/js/main.js
@@ -11,5 +11,5 @@ window.addEventListener("scroll", () => {
     let scrollY = window.scrollY;
     let opacity = Math.min(scrollY / 200, 1); //giới hạn từ 0 đến 1
 
-    header.style.background = `rgba(51, 51, 51, ${opacity})`;
+    header.style.background = `rgba(0, 0, 0, ${opacity})`;
 });
