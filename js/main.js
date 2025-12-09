b1769e7c757e20ed78d5ca02c26c75dd3e11947c
NguyenHung2803 <nguyenntiennhungg280306@gmail.com>
2025-12-09 20:55:24 +0700

    upFilm

--- js/main.js
+++ js/main.js
@@ -30,7 +30,8 @@ const moviesData = [
         duration: "1h 50m",
         desc: "Câu chuyện về một chàng trai trẻ với ước mơ trở thành thợ săn rồng, nhưng định mệnh lại đưa đẩy anh đến tình bạn bất ngờ với một chú rồng. Giống như bộ phim hay gốc từ năm 2010, Bí Kíp Luyện Rồng phiên bản live-action lần này diễn ra trên Đảo Berk, nơi người Viking và rồng đã là kẻ thù không đội trời chung trong nhiều thế hệ. Nhưng khi Hiccup (Mason Thames) – con trai của Tù trưởng Stoick the Vast đi ngược lại truyền thống hàng thế kỷ để kết bạn với Toothless - một con rồng Night Fury, chính giây phút đó anh ấy đã phá vỡ quy tắc vốn có của cộng đồng và mở ra sự căng thẳng tột cùng cho cả người Viking và rồng.",
         img: "images/dragon.jpg",
-        trailerId: "tCrdzzpk6qg" //Chỉ lấy mã sau chữ v=
+        trailerId: "tCrdzzpk6qg", //Chỉ lấy mã sau chữ v=
+        link: "xp-bikipluyenrong.html"
     },
     {
         title: "Avengers: Cuộc Chiến Vô Cực",
@@ -39,7 +40,8 @@ const moviesData = [
         duration: "2h 29m",
         desc: "Avengers: Cuộc Chiến Vô Cực đánh dấu màn ảnh trận chiến cuối cùng hoành tráng và khốc liệt nhất. Biệt đội Avengers, cùng với các đồng minh siêu anh hùng sẽ phải đối mặt với thử thách cam go nhất: chống lại kẻ thù hùng mạnh Thanos. Tham vọng hủy diệt toàn bộ vũ trụ của Thanos buộc các siêu anh hùng phải hy sinh tất cả để bảo vệ sự sống.",
         img: "images/avengers.jpg",
-        trailerId: "k2b2Jb24hQc"
+        trailerId: "k2b2Jb24hQc",
+        link: "xp-avenger.html"
     },
     {
         title: "Thám Tử Lừng Danh Conan Movie 28: Dư Ảnh của Độc Nhãn",
@@ -48,7 +50,8 @@ const moviesData = [
         duration: "1h 49m",
         desc: "Trên những ngọn núi tuyết của Nagano, một vụ án bí ẩn đã đưa Conan và các thám tử quay trở lại quá khứ. Thanh tra Yamato Kansuke - người đã bị thương nặng trong một trận tuyết lở nhiều năm trước - bất ngờ phải đối mặt với những ký ức đau thương của mình trong khi điều tra một vụ tấn công tại Đài quan sát Nobeyama. Cùng lúc đó, Mori Kogoro nhận được một cuộc gọi từ một đồng nghiệp cũ, tiết lộ mối liên hệ đáng ngờ giữa anh ta và vụ án đã bị lãng quên từ lâu. Sự xuất hiện của Morofushi Takaaki, cùng với những nhân vật chủ chốt như Amuro Tooru, Kazami và cảnh sát Tokyo, càng làm phức tạp thêm cuộc điều tra. Khi quá khứ và hiện tại đan xen, một bí ẩn rùng rợn dần dần được hé lộ - và ký ức của Kansuke nắm giữ chìa khóa cho mọi thứ.",
         img: "images/connan28.jpg",
-        trailerId: "5bKBCTpbTb4" 
+        trailerId: "5bKBCTpbTb4",
+        link: "xp-conan.html"
     },
     {
         title: "Zootopia 2",
@@ -57,7 +60,8 @@ const moviesData = [
         duration: "1h 50m",
         desc: "Hai thám tử Judy Hopps và Nick Wilde bước vào hành trình truy tìm một sinh vật bò sát bí ẩn vừa xuất hiện tại Zootopia và khiến cả vương quốc động vật bị đảo lộn. Để phá được vụ án, Judy và Nick buộc phải hoạt động bí mật tại những khu vực mới lạ của thành phố – nơi mối quan hệ đồng nghiệp của họ bị thử thách hơn bao giờ hết.",
         img: "images/zootopia2.jpg",
-        trailerId: "BjkIOU5PhyQ&t=8s" 
+        trailerId: "BjkIOU5PhyQ&t=8s",
+        link: "xp-Zootopia.html"
     },
     {
         title: "Avatar 3: Lửa và Tro Tàn",
@@ -66,7 +70,8 @@ const moviesData = [
         duration: "3h 10m",
         desc: "Jake Sully đối mặt với tộc người Na'vi lửa đầy hung hãn...",
         img: "images/avarta3.jpg",
-        trailerId: "nb_fFj_0rq8"
+        trailerId: "nb_fFj_0rq8",
+        link: "xp-avarta3.html"
     }
 ];
 
@@ -88,7 +93,13 @@ function updateInfo(index) {
     const embedUrl = `https://www.youtube.com/embed/${movie.trailerId}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&loop=1&playlist=${movie.trailerId}`;
     
     panelVideo.src = embedUrl;
-
+// 1. Tìm nút Xem Ngay trong panel
+    const watchButton = document.querySelector('#movie-info-panel .btn-primary');
+    
+    // 2. Gán đường dẫn từ data vào nút
+    if (watchButton) {
+        watchButton.href = movie.link; 
+    }
     const textContent = document.querySelector('.panel-text');
     const imgContent = document.querySelector('.panel-poster');
 
@@ -127,7 +138,8 @@ const animeRankData = [
         year: "2024",
         desc: "Trong cuộc chiến cuối cùng định đoạt số phận thế giới, Eren Yeager đã giải phóng sức mạnh tối thượng của các Titan. Dẫn đầu đội quân Titan Đại hình khổng lồ, Eren quyết tâm hủy diệt mọi kẻ thù đe dọa đến quê hương Eldia",
         videoId: "3xNH23QkNpk",
-        img: "images/aot.jpg"
+        img: "images/aot.jpg",
+        link: "xp-aot.html"
     },
     {
         id: 1,
@@ -140,7 +152,8 @@ const animeRankData = [
         year: "2025",
         desc: "Cuộc chiến cuối cùng bắt đầu! Tanjiro và Sát Quỷ Đoàn thâm nhập vào Vô Hạn Thành.",
         videoId: "rf0hW__Skow",
-        img: "images/vohanthanh.jpg"
+        img: "images/vohanthanh.jpg",
+        link: "xp-demon.html"
     },
     {
         id: 2,
@@ -153,7 +166,8 @@ const animeRankData = [
         year: "2025",
         desc: "Nobita và Doraemon vô tình lạc vào một thế giới trung cổ tồn tại bên trong tranh.Chú Thuật Hồi Chiến 0",
         videoId: "8QkHWlrHqlE",
-        img: "images/jujus.jpg"
+        img: "images/jujus.jpg",
+        link: "xp-jujutsu.html"
     },
     {
         id: 3,
@@ -166,7 +180,8 @@ const animeRankData = [
         year: "2024",
         desc: "Sung Jin-woo tiếp tục hành trình thăng cấp điên cuồng tại các hầm ngục cấp S.",
         videoId: "OdQrlp3uYQM",
-        img: "images/solo.jpg"
+        img: "images/solo.jpg",
+        link: "xp-solo.html"
     },
     {
         id: 4,
@@ -179,7 +194,8 @@ const animeRankData = [
         year: "2019",
         desc: "Tác giả đã giải thích rằng movie thứ hai này có thể được mô tả như một dạng kết của Boku no Hero Academia",
         videoId: "iaMLBWj6HjI",
-        img: "images/heroacademia.jpg"
+        img: "images/heroacademia.jpg",
+        link: "xp-hero.html"
     }
 ];
 let currentKingIndex = 0;
@@ -199,6 +215,13 @@ function renderInterface() {
     const videoUrl = `https://www.youtube.com/embed/${kingData.videoId}?autoplay=1&mute=1&controls=0&showinfo=0&rel=0&loop=1&playlist=${kingData.videoId}`;
     
     iframeEl.src = videoUrl;
+   // 1. Tìm nút Xem Ngay trong khu vực anime-king
+    const watchButton = document.querySelector('.anime-king .btn-watch');
+
+    // 2. Gán link từ dữ liệu vào nút
+    if (watchButton) {
+        watchButton.href = kingData.link;
+    }
     const listContainer = document.getElementById('anime-list-container');
     listContainer.innerHTML = ''; 
 
